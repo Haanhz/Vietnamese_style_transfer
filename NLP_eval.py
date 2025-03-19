fd857223a939fb7d971d7308a78efaf459518a44
Haanhz <hannahbk476@gmail.com>
2025-03-19 22:19:05 +0700

    8

--- NLP_eval.py
+++ NLP_eval.py
@@ -69,7 +69,7 @@ def predict_api():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("test_Nlp.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Lấy PORT từ env, mặc định 5000
